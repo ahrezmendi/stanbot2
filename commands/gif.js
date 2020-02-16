2b3153d79ee37175dfc0f8bfb87b3c43b158aea8
Michael Stone <ahrezmendi@gmail.com>
2020-02-16 13:09:16 -0800

    Shhhh, it's a sekret.

--- commands/gif.js
+++ commands/gif.js
@@ -5,7 +5,10 @@ const reactions = new Discord.Collection()
     .set('letsgo', 'https://gfycat.com/achinguniquebird')
     .set('pacha', '✋👌')
     .set('approve', 'https://tenor.com/view/seal-approval-gif-5057575')
-    .set('sealion', 'https://tenor.com/view/bait-thatsbait-gif-5055384');
+    .set('sealion', 'https://tenor.com/view/bait-thatsbait-gif-5055384')
+    .set('amn', `"Sorry losers and haters, but my I.Q. is one of the highest -and you all know it! Please don't feel so stupid or insecure,it's not your fault"\n\n🍆🍆🍆\n\nHow dare you.`)
+    .set('eastwardflea', `I'm trash. Uninstalled.\n\nS2 Urien was fine.\nAkatsuki is fine.\nS1 Android 16 was fine.\n\nFuck printers.`)
+    .set('bnice', `LET'S FUCKING GO!!!!!!!!\n\n<new gif with text overlayed>\n\nWhat does the crystal ball say?`);
 
 module.exports = {
     name: 'gif',
